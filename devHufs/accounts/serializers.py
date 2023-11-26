3c35e001175e6c102abdd5fa4f38dd96713c042b
hyobin-yang <hyobin3026@naver.com>
2023-11-27 01:08:37 +0900

    [Feat]: 구글 로그인 수정

---
+++ devHufs/accounts/serializers.py
@@ -0,0 +1,10 @@
+# from .models import User
+# from rest_framework import serializers
+
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['id', 'username', 'profile_img', 'email', 'student_num', 'stack']
+        
+
--- devHufs/devHufs/urls.py
+++ devHufs/devHufs/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', goologin.views.home, name='home')
     path('accounts/', include('allauth.urls')),
+    # path('mypage/', include('mypage.urls')),
+    # path('', include('iamport.urls')),
+    # path('', include('store.urls')),
 ]
+
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
