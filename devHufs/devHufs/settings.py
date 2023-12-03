968847c228dc99e33b30a5e617b6f21a871502d8
hyobin-yang <hyobin3026@naver.com>
2023-12-03 16:58:58 +0900

    [Feat]: 배포 설정

--- devHufs/devHufs/settings.py
+++ devHufs/devHufs/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-backend-1gksli2alpp0ksdw.sel4.cloudtype.app', '127.0.0.1']
 
 
 # Application definition
