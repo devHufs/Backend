dfdbcc38293f090d7dc62dab78a24ea124dfdeed
hyobin-yang <hyobin3026@naver.com>
2023-12-03 16:51:25 +0900

    [Feat]: 구글 로그인

--- devHufs/accounts/models.py
+++ devHufs/accounts/models.py
@@ -1,15 +1,15 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from allauth.socialaccount.models import SocialApp
+#from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# from allauth.socialaccount.models import SocialApp
 
-class GoogleLogin(models.Model):
-    platform = models.CharField(max_length=20, default = 0)
+# class GoogleLogin(models.Model):
+#     platform = models.CharField(max_length=20, default = 0)
 
-    class Meta:
-        db_table = 'google_login'
+#     class Meta:
+#         db_table = 'google_login'
 
-class User(models.Model):
-    social = models.ForeignKey(GoogleLogin, on_delete=models.CASCADE, max_length=20, blank=True, default=1)
-    social_login_id = models.CharField(max_length=50, blank=True)
+# class User(models.Model):
+#     social = models.ForeignKey(GoogleLogin, on_delete=models.CASCADE, max_length=20, blank=True, default=1)
+#     social_login_id = models.CharField(max_length=50, blank=True)
 
 
