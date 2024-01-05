c531fc9628e7aeebf83b973c9f29b54f083db49e
hyobin-yang <hyobin3026@naver.com>
2024-01-06 04:41:29 +0900

    배포 수정

--- devHufs/devHufs/settings.py
+++ devHufs/devHufs/settings.py
@@ -6,7 +6,7 @@ DEBUG = True
 #ALLOWED_HOSTS = ['port-0-backend-1gksli2alpp0ksdw.sel4.cloudtype.app', '127.0.0.1']
 #ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
 
-ALLOWED_HOSTS = ['devhufs-devhufs.koyeb.app/', '127.0.0.1']
+ALLOWED_HOSTS = ['devhufs-devhufs.koyeb.app', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
