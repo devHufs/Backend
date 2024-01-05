6026ca438e040f51630f8b9a8a22ed1e7d9a5e60
hyobin-yang <hyobin3026@naver.com>
2024-01-06 04:19:04 +0900

    배포 수정

--- devHufs/devHufs/settings.py
+++ devHufs/devHufs/settings.py
@@ -3,8 +3,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
-ALLOWED_HOSTS = ['port-0-backend-1gksli2alpp0ksdw.sel4.cloudtype.app', '127.0.0.1']
+#ALLOWED_HOSTS = ['port-0-backend-1gksli2alpp0ksdw.sel4.cloudtype.app', '127.0.0.1']
+# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
 
+ALLOWED_HOSTS = ['https://devhufs-devhufs.koyeb.app/', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
@@ -73,25 +75,24 @@ WSGI_APPLICATION = 'devHufs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'devHufs_test',
-        #'BASE_DIR' : 'db.sqlite3',
-        'USER' : 'root',
-        'PASSWORD' : '2311',
-        'HOST' : 'svc.sel4.cloudtype.app',
-        'PORT' : '30087'
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'devHufs_test',
+#         'USER' : 'root',
+#         'PASSWORD' : '2311',
+#         'HOST' : 'svc.sel4.cloudtype.app',
+#         'PORT' : '30087'
 #     }
 # }
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
@@ -130,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
