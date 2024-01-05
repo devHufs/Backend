9a7fe17df94c8e7b8febf6923e08410695d97317
Chaeyoung714 <202102760@hufs.ac.kr>
2024-01-05 21:45:33 +0900

    [Fix]: settings.py 임시 주석

--- devHufs/devHufs/settings.py
+++ devHufs/devHufs/settings.py
@@ -73,24 +73,24 @@ WSGI_APPLICATION = 'devHufs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'devHufs_test',
-        #'BASE_DIR' : 'db.sqlite3',
-        'USER' : 'root',
-        'PASSWORD' : '2311',
-        'HOST' : 'svc.sel4.cloudtype.app',
-        'PORT' : '30087'
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'devHufs_test',
+#         #'BASE_DIR' : 'db.sqlite3',
+#         'USER' : 'root',
+#         'PASSWORD' : '2311',
+#         'HOST' : 'svc.sel4.cloudtype.app',
+#         'PORT' : '30087'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
