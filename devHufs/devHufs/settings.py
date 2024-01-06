14aa001cf35c4122baeaab2dc808fd757b4702c6
hyobin-yang <hyobin3026@naver.com>
2024-01-07 02:25:29 +0900

    배포 오류 수정

--- devHufs/devHufs/settings.py
+++ devHufs/devHufs/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # CORS 에러
