44dd72fb775bb2795422a06d52851900cf46dc00
Chaeyoung714 <202102760@hufs.ac.kr>
2024-01-11 22:00:11 +0900

    link, job, stack필드, filter함수 주석처리

--- devHufs/home/views.py
+++ devHufs/home/views.py
@@ -154,14 +154,14 @@ def search(request, search):
         return Response("No result")
     
 #게시물 필터링 (기술스택)
-@api_view(['GET'])
-def search(request, search_stack):
-    contents = Content.objects.filter(Q(stack__contains = search_stack)) #stack 데이터타입 보고 수정 예정
-    if contents.exists():
-        serializer = ContentSerializer(contents, many=True)
-        return Response(serializer.data)
-    else:
-        return Response("No result")
+# @api_view(['GET'])
+# def filter(request, search_stack):
+#     contents = Content.objects.filter(Q(stack__contains = search_stack)) #stack 데이터타입 보고 수정 예정
+#     if contents.exists():
+#         serializer = ContentSerializer(contents, many=True)
+#         return Response(serializer.data)
+#     else:
+#         return Response("No result")
 
 
 #---------아래는 User모델 추가 후 주석풀기 필요--------------------
